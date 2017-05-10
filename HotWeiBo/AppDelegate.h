76651389c96063f1f620196f740fd51523eed81c
xiufengfan <1457103610@qq.com>
2017-05-10 14:05:17 +0800

    新浪微博
    1.创建项目；
    2.导入UI元素

---
+++ HotWeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotWeiBo
+//
+//  Created by mac on 2017/5/10.
+//  Copyright © 2017年 XFF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotWeiBo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HotWeiBo
+//
+//  Created by mac on 2017/5/10.
+//  Copyright © 2017年 XFF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
